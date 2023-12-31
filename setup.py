3bb578aa5c506ab4f1dd5ce2b1a2e3984ad47d74
Deependra Verma <deependra.verma00@gmail.com>
2023-12-31 07:38:06 +0530

    Setup for local packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical Chatbot",
+    version = "0.0.0",
+    author = "Deependra Verma",
+    author_email = "deependra.verma00@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
